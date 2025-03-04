ab48da1eb12ce3503f29408d65b986b5b6515670
Anindyo Gupta <anindyo100@yahoo.com>
2025-03-04 09:22:13 +0530

    Updates

--- example-xml2json/conversion.js
+++ example-xml2json/conversion.js
@@ -1,3 +1,11 @@
 var recursive = require("recursive-readdir");
 var parser = require("xml-js");
 const fs = require("fs-extra");
+
+var file_array;
+
+// Reads the `files` directory recursively and places individual file names into the `file_array` array.
+
+recursive("files", function (err, file_array) {
+    console.log("Found these files", file_array);
+  });
