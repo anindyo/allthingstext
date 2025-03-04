91124abc55d53f427d0d4ce7b4b459197be815e8
Anindyo Gupta <anindyo100@yahoo.com>
2025-03-04 11:57:37 +0530

    Update conversion.js

--- example-xml2json/conversion.js
+++ example-xml2json/conversion.js
@@ -2,10 +2,41 @@ var recursive = require("recursive-readdir");
 var parser = require("xml-js");
 const fs = require("fs-extra");
 
-var file_array;
+//var file_array = [];
+var xml_content = '';
 
 // Reads the `files` directory recursively and places individual file names into the `file_array` array.
+/*
+recursive('files', function (err, files) {
+    console.log(files);
+    files = file_array;
+  }
 
-recursive("files", function (err, file_array) {
-    console.log("Found these files", file_array);
-  });
+);
+*/
+recursive("files").then(
+    function(files) {
+        console.log(files);
+        for (i=0;i<files.length;i++) {
+            xml_content = fs.readFileSync(files[i], 'utf8');
+            
+            console.log(xml_content);
+        };
+ 
+ 
+
+    },
+    function(error) {
+      console.error("something exploded", error);
+    }
+  );
+
+
+
+
+/*
+for (i=0;i<file_array.length;i++) {
+    xml_content = fs.readFileSync(file_array[i], 'utf8');
+    console.log(xml_content);
+};
+*/
